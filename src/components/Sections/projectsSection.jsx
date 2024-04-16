5ffd434b73b6f255d06bc4c9a454dda1ee070b2c
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-04-16 15:55:12 +0700

    project section 20%. cihuy.

--- src/components/Sections/projectsSection.jsx
+++ src/components/Sections/projectsSection.jsx
@@ -1,6 +1,12 @@
+'use client'
 import { motion } from "framer-motion";
 import HomeButton from "../Button/homeButton";
 import Link from "next/link";
+import dataProject from "@/../public/json/projectsData.json";
+import testingImage from "@/../public/img/projectsImage/photo.png"
+import Image from "next/image";
+
+import githubIcon from "@/../public/icon/github-icon.png"
 
 export default function ProjectsSection({fontLexend}) {
     return(
@@ -21,8 +27,42 @@ export default function ProjectsSection({fontLexend}) {
                     <HomeButton value="See More" background={"bg-gray-600"} textCol={"text-white"} bgHover="hover:bg-white" textHov={"hover:text-gray-600"} />
                 </Link>
             </div>
-            <div className="border-2 border-green-400" >
-                
+            <div className="border-2 border-green-500 p-4 w-full flex">
+                <ProjectCard />
+            </div>
+        </div>
+    )
+}
+
+
+function ProjectCard() {
+
+    return(
+        <div className="border-2 border-black p-2 flex items-center justify-center">
+            <div className="p-2 border-black rounded-full">
+                <p>Prev</p>
+            </div>
+            <div className="border-2 border-yellow-400 relative">
+                <motion.div 
+                    className="absolute top-0 right-0 w-14"
+                    initial={{x: 0, scale: 0, opacity: 0}}
+                    whileInView={{x: 0, scale: 1, opacity: 1}}
+                    transition={{delay: 0.5, type: "spring"}}
+                    whileHover={{scale: 1.2, transition: {delay: 0, type: "spring"}}}
+                >
+                    <Link href={""}>
+                        <Image src={githubIcon} alt="githubIcon" width={50} height={50} />
+                    </Link>
+                </motion.div>
+                <motion.div 
+                    className="md:min-w-72 w-full"
+                >
+                    <Image src={testingImage} alt="testing" width={400} height={400} />
+                </motion.div>
+
+            </div>
+            <div className="p-2 border-black rounded-full">
+                <p>Next</p>
             </div>
         </div>
     )
