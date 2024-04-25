f91cf95971299b5b846cb4d9e7e6fd578fcfcbae
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-04-25 21:38:23 +0700

    responsive main image in about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,9 @@ import { motion } from "framer-motion";
 import { Lexend_Deca } from 'next/font/google';
 
 // images
-import mySelfPict from "@/../public/img/myself2.jpg"
+import mySelfPict1 from "@/../public/img/myself2.jpg"
+import mySelfPict1Copy from "@/../public/img/myself2copy.jpg"
+
 
 const lexendDeca = Lexend_Deca({
     weight: "800",
@@ -15,23 +17,44 @@ const lexendDeca = Lexend_Deca({
 
 export default function AboutPage() {
     return(
-        <div className="w-full mx-auto px-10 sm:px-24 overflow-hidden" >
-            <div className="w-full border-2 border-black flex justify-between">
+        <div className="w-full mx-auto px-10 pt-20 lg:pt-0 sm:px-24 overflow-hidden" >
+            <div className="w-full border-2 border-black lg:flex relative justify-between">
+
+                {/* Image large width */}
                 <motion.div
-                    className={`border-2 border-red-500 w-3/4 min-w-80 relative hidden md:block`}
+                    className={`border-2 border-red-500 relative hidden lg:block w-3/4 h-[100vh]`}
                     initial={{opacity: 0, scale: 0.8}}
                     whileInView={{opacity: 1, scale: 1}}
                     transition={{delay: 0.3, type:"spring"}}
                 >
-                    <Image src={mySelfPict} height={800} weight={800} className="grayscale hover:grayscale-0 duration-300 ease-in-out placeholder:blur min-w-full object-cover" />
+                    <Image src={mySelfPict1} layout="fill" placeholder="blur" alt="Irzi Rahmatullah" objectFit="cover" className="grayscale hover:grayscale-0 duration-300 ease-in-out placeholder:blur min-w-full object-cover" />
 
                     <motion.div
-                        className={`mx-auto text-center absolute bottom-5 left-10 p-2 px-6 rounded-3xl bg-gray-200 bg-opacity-50`}
+                        className={`mx-auto text-center absolute bottom-5 p-2 px-6 md:rounded-2xl lg:rounded-3xl bg-gray-200 bg-opacity-50`}
                         initial={{x: -100, opacity: 0}}
                         whileInView={{x:0, opacity: 1}}
                         transition={{delay: 0.7, type:"spring"}}
                     >
-                        <h1 className={`text-4xl sm:text-6xl text-black ${lexendDeca.className} font-bold`}>About Me</h1>
+                        <h1 className={`text-3xl md:text-4xl lg:text-5xl text-black ${lexendDeca.className} font-bold`}>About Me</h1>
+                    </motion.div>
+                </motion.div>
+
+                {/* Image medium width */}
+                <motion.div
+                    className={`lg:hidden border-2 border-red-500 absolute z-0 h-[400px] w-full `}
+                    initial={{opacity: 0, scale: 0.8}}
+                    whileInView={{opacity: 1, scale: 1}}
+                    transition={{delay: 0.3, type:"spring"}}
+                >
+                    <Image src={mySelfPict1Copy} layout="fill" placeholder="blur" alt="Irzi Rahmatullah" objectFit="cover" className="grayscale hover:grayscale-0 duration-300 ease-in-out placeholder:blur min-w-full object-cover" />
+
+                    <motion.div
+                        className={`mx-auto text-center absolute bottom-5 p-2 px-6 md:rounded-2xl lg:rounded-3xl bg-gray-200 bg-opacity-50`}
+                        initial={{x: -100, opacity: 0}}
+                        whileInView={{x:0, opacity: 1}}
+                        transition={{delay: 0.7, type:"spring"}}
+                    >
+                        <h1 className={`text-3xl md:text-4xl lg:text-5xl text-black ${lexendDeca.className} font-bold`}>About Me</h1>
                     </motion.div>
                 </motion.div>
                 <div className="pt-20 sm:p-14 border-2 border-green-500 w-full">
@@ -74,7 +97,7 @@ export default function AboutPage() {
                     whileInView={{opacity: 1, scale: 1}}
                     transition={{delay: 0.3, type:"spring"}}
                 >
-                    <Image src={mySelfPict} height={800} weight={800} className="grayscale hover:grayscale-0 duration-300 ease-in-out placeholder:blur w-full object-cover" />
+                    <Image src={mySelfPict1} height={800} weight={800} className="grayscale hover:grayscale-0 duration-300 ease-in-out placeholder:blur w-full object-cover" />
             </motion.div>
             </div>
         </div>
