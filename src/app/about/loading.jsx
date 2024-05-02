130cd26c67e59af9737586a9f205473bda12bdfa
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-05-03 00:34:51 +0700

    add loader

---
+++ src/app/about/loading.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+	return (
+		<>
+			(
+			<div className="fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-gray-300 z-[999]">
+                <div className="loader"></div>
+            </div>
+			)
+		</>
+	);
+}
