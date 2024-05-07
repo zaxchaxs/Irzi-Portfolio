ece5111a080b1abe355c17053197340591481fa7
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-05-07 22:22:06 +0700

    update project card

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -4,6 +4,7 @@ import { Lexend_Deca } from 'next/font/google';
 import dataProject from "@/../public/json/projectsData.json";
 import ProjectCard from "@/components/ProjectCards";
 import Link from "next/link";
+import SingleUnderline from "@/components/Underlines/singleUnderline";
 
 const lexendDeca = Lexend_Deca({
     weight: "800",
@@ -16,15 +17,16 @@ export default function ProjectsPage() {
       <div className="p-10 sm:px-24 mx-auto pt-20 overflow-hidden">
         <div className="w-full">
           <motion.div
-            className={`text-2xl text-black ${lexendDeca.className} text-center p-2 w-full`}
+            className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-black ${lexendDeca.className} text-center p-2 w-full`}
             initial={{ y: -100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ type: "spring", delay: 0.3 }}
           >
             <h1>My Projects</h1>
+            <SingleUnderline />
           </motion.div>
           <motion.div
-            className="text-base p-2 text-justify"
+            className="text-base md:text-lg p-2 text-justify sm:mb-10 md:mb-20"
             initial={{ x: -100, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ type: "spring", delay: 0.5 }}
@@ -41,13 +43,13 @@ export default function ProjectsPage() {
         </div>
 
         {/* Project Cards */}
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-4">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 pt-4">
             {
                 dataProject.data.map(e => <ProjectCard key={e.id} title={e.title} desc={e.desc} tech={e.tech} srcCode={e.source} srcImg={e.image} year={e.year} />)
             }
         </div>
         <div className="w-full flex justify-center py-10">
-            <div className={`w-fit my-4 p-2 text-base ${lexendDeca.className} underline shadow-xl rounded-md`}>
+            <div className={`w-fit my-4 p-2 text-xs sm:text-base ${lexendDeca.className} underline shadow-xl rounded-md`}>
                 <Link href={"https://github.com/zaxchaxs"} target="blank">See More Projects I've Created</Link>
             </div>
         </div>
--- src/components/ProjectCards/index.jsx
+++ src/components/ProjectCards/index.jsx
@@ -1,43 +1,53 @@
 import Image from "next/image"
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProjectCard({title, desc, srcImg, srcCode, year, tech }) {
     const [isReadMore, setIsReadMore] = useState(desc.length > 100 ? true : false);
+    const [sliceDesc, setSliceDesc] = useState(100);
+
+    useEffect(() => {
+      if(window.innerWidth < 1024) { 
+        setSliceDesc(80); //lg
+      } else if(window.innerWidth < 768) {
+        setSliceDesc(70); //md
+      } else if(window.innerWidth < 640) {
+        setSliceDesc(60); //sm
+      }
+    }, [])
 
     const readMoreHandler = () => {
         setIsReadMore(!isReadMore);
     }
-
     return (
-      <div className="p-2 rounded-2xl bg-gray-600 shadow-xl w-full h-fit md:min-h-[80vh]">
+      <div className="p-2 rounded-2xl relative bg-gray-600 shadow-xl w-full min-h-fit sm:min-h-[90vh] md:min-h-[100vh] lg:min-h-[78vh] h-fit">
         <div className="rounded-2xl relative h-[25vh] sm:h-[30vh] md:h-[35vh]">
           <Image
             src={srcImg}
             alt={title}
             layout="fill"
-            objectFit=""
             className="rounded-2xl w-full"
           />
         </div>
-        <div className="p-2 text-slate-200 w-full border-2 border-black">
-          <h1 className="p-2 text-lg font-bold">{title}</h1>
-          <p>{isReadMore ? desc.slice(0, 100) + "..." : desc}</p>
+        <div className="p-2 text-slate-200 sm:min-h-[56vh] md:min-h-[65vh] lg:min-h-[40vh] items-end relative flex flex-col justify-between">
+          <div>
+            <h1 className="p-2 text-lg font-bold">{title}</h1>
+            <p>{isReadMore ? desc.slice(0, sliceDesc) + "..." : desc}</p>
 
-          {/* <Description desc={desc} /> */}
-          {desc.length > 100 ? (
-            <ReadMoreBtn
-              onReadMoreClick={readMoreHandler}
-              isReadMore={isReadMore}
-            />
-          ) : (
-            ""
-          )}
-
-          <div className="my-3 w-full align-bottom border-2 border-black">
+            {/* <Description desc={desc} /> */}
+            {desc.length > 100 ? (
+              <ReadMoreBtn
+                onReadMoreClick={readMoreHandler}
+                isReadMore={isReadMore}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="mt-3 w-full">
             <Link
               href={srcCode}
-              className="p-1 px-4 bg-slate-200 text-black w-fit rounded-full font-bold"
+              className="p-1 px-4 bg-slate-200 text-black w-fit rounded-xl font-bold"
               target="blank"
             >
               View
