738a12225f5c8e738f8c178ba6d8c9e6c83516cf
Irzi <134607123+zaxchaxs@users.noreply.github.com>
2025-03-28 20:16:28 +0700

    Fix copyright year

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,8 @@ export default function Footer() {
     return(
         <div className="w-full text-center text-sm md:text-base">
             <p>This Website is built with <a className="underline font-bold text-black" href="https://nextjs.org/" target="blank">Next.js</a>.</p>
-            <p>© Irzi 2024. <b>All right reserved.</b></p>
+            <p>{`© Irzi Rahmatullah  ${new Date().getFullYear()}`}</p>
+            <p><b>All right reserved.</b></p>
         </div>
     )
 }
