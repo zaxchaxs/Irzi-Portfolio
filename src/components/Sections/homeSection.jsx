4669bc8a541ca7303e602a6e134b2561c51a0dfe
Irzi <134607123+zaxchaxs@users.noreply.github.com>
2025-07-07 20:01:15 +0700

    Update CV

--- src/components/Sections/homeSection.jsx
+++ src/components/Sections/homeSection.jsx
@@ -74,7 +74,7 @@ export default function HomeSection({fontLexend}) {
               </p>
             </div>
             <div className="p-2 sm:p-4 flex justify-center items-center md:justify-start">
-              <Link href={"/docs/Irzi_Rahmatullah-CV.pdf"}>
+              <Link href={"/docs/Irzi_Rahmatullah-CV.pdf"} target="_blank">
                 <HomeButton
                   value="Download CV"
                   background={"bg-gray-600"}
